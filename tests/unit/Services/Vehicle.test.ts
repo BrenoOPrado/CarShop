ec9edac7c2f78e0c116852483eab4b0e6c544ec4
BrenoOPrado <breno.o.prado@hotmail.com>
2023-01-13 18:43:59 -0300

    Inicio da criação de testes unitarios :turtle:

---
+++ tests/unit/Services/Vehicle.test.ts
@@ -0,0 +1,35 @@
+import { Model } from 'mongoose';
+import Sinon from 'sinon';
+import Car from '../../../src/Domains/Car';
+import ICar from '../../../src/Interfaces/ICar';
+import CarService from '../../../src/Services/CarService';
+
+describe('Deveria criar uma transferência TRIX', function () {
+  it('Deveria criar uma transferência TRIX com SUCESSO', async function () {
+    const carInput: ICar = {
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+    const carOutput: Car = new Car({
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    });
+
+    Sinon.stub(Model, 'create').resolves(carOutput);
+    
+    const service = new CarService();
+    const result = await service.create(carInput);
+    
+    expect(result).to.be.deep.equal(carOutput);
+  });
+});
